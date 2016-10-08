889b63b097b3d4a8d07e8f4558a6acd78912d009
oriolpericas <oriolpericas@gmail.com>
2016-10-08 11:09:01 +0200

    Linked menu Add/Edit with json

--- src/app/restaurant/restaurant-menu.component.ts
+++ src/app/restaurant/restaurant-menu.component.ts
@@ -1,8 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router, Params} from '@angular/router';
+
+import {Menu} from './menu';
+import {MenuService} from './menu.service';
 
 @Component({
     selector: 'restaurant-menu',
     templateUrl: 'restaurant-menu.component.html'
 })
 
-export class RestaurantMenuComponent { }
+export class RestaurantMenuComponent implements OnInit {
+    
+   menu = new Menu();
+    
+   constructor(private _activatedRoute: ActivatedRoute, private _router: Router, private _menuService: MenuService) { }
+       
+   ngOnInit() {
+                this._menuService.getLastMenu().then(menu => {
+                this.menu = menu;});
+        }
+        
+}
