3765e5b65f9fc6f12071e72ca04db0ee002a020f
jarts <jordiazal@gmail.com>
2016-10-08 09:14:40 +0200

    Subida pantalla Menus

--- src/app/menu.component.ts
+++ src/app/menu.component.ts
@@ -1,8 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+//import {MdIcon, MdIconRegistry} from '@angular2-material/icon/icon';
+//import {MdIconRegistry} from '@angular/material';
+
 @Component({
     selector: 'menu',
     templateUrl: 'menu.component.html'
+  //  viewProviders: [MdIconRegistry],
+  //  directives: [MdIcon]
 })
 
-export class MenuComponent { }
+export class MenuComponent { 
+
+	url:any = "http://www.abc.es/Media/201407/10/tortilla-patata--644x362.jpg";
+
+	getAlert(newMessage: any){
+        alert(newMessage);
+    }
+
+    getDishPreview(){
+    	alert("this is cool");	
+    }
+
+    loadImagePreview(newImage: any) {
+        this.url=newImage;
+        //alert("IMAGE "+newImage + " var component="+this.url);
+    }
+
+}
