e7ee1ba15da868f26a505751ca011ef8f272f928
Alejandro Sanz <alejandro@blissbooker.com>
2016-10-08 11:15:49 +0200

    CHANGE component name

--- src/app/menu.component.ts
+++ src/app/menu.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 //import {MdIconRegistry} from '@angular/material';
 
 @Component({
-    selector: 'menu',
+    selector: 'client-menu',
     templateUrl: 'menu.component.html'
   //  viewProviders: [MdIconRegistry],
   //  directives: [MdIcon]
